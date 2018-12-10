fac0593e1e567b82afb7d52d7af5213824d23437
J. Aravind <aravindjayaramanagri@gmail.com>
2018-12-10 13:19:38 +0530

    Add example data

---
+++ R/gcdata.R
@@ -0,0 +1,13 @@
+#' Germination Count Data
+#'
+#' An example germination count dataset. It includes germination count data over
+#' 14 days from five genotypes (G1 to G5) in three replications
+#'
+#' @format A data frame with 17 columns: \describe{ \item{Genotype}{The
+#'   genotype.} \item{Rep}{Replication.} \item{Day1 to Day14}{Number of seeds
+#'   that germinated (germination counts) on a particular day.} \item{Total
+#'   Seeds}{Total number of seeds tested.} }
+#'
+#' @examples
+#'
+"gcdata"
