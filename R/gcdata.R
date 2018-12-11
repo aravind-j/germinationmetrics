cc7d7c82722e9f5e75ebd522ab6b281a85bb1abc
J. Aravind <aravindjayaramanagri@gmail.com>
2018-12-11 20:24:18 +0530

    Addes examples

--- R/gcdata.R
+++ R/gcdata.R
@@ -10,4 +10,13 @@
 #'
 #' @examples
 #'
+#' data(gcdata)
+#'
+#' counts.per.intervals <- c("Day1", "Day2", "Day3", "Day4", "Day5",
+#'                           "Day6", "Day7", "Day8", "Day9", "Day10", "Day11",
+#'                           "Day12", "Day13", "Day14")
+#' germination.indices(gcdata, total.seeds.col = "Total Seeds",
+#'                     counts.intervals.cols = counts.per.intervals,
+#'                     intervals = 1:14, partial = TRUE, max.int = 5)
+#'
 "gcdata"
