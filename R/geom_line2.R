65ad1f9ea696d9c059ec0c514c105d10807dafb7
J. Aravind <aravindjayaramanagri@gmail.com>
2024-08-04 08:45:36 +0530

    Added header

--- R/geom_line2.R
+++ R/geom_line2.R
@@ -1,3 +1,20 @@
+### This file is part of 'germinationmetrics' package for R.
+
+### Copyright (C) 2017-2023, ICAR-NBPGR.
+#
+# germinationmetrics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# germinationmetrics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# A copy of the GNU General Public License is available at
+# https://www.r-project.org/Licenses/
 
 #' @importFrom rlang `%||%`
 #' @export
@@ -334,9 +351,6 @@ GeomLine2 <- ggproto("GeomLine2", GeomPath2,
                        flip_data(data, params$flipped_aes)
                      }
 )
-
-
-
 
 
 # Helper functions from ggplot2
